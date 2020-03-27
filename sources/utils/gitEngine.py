753a7cc5dce086442c1c020c28cfb94870f5d1c5
Samuel Hornus <samuel.hornus@gmail.com>
2020-03-27 10:46:57 +0100

    use run() instead of call()

--- sources/utils/gitEngine.py
+++ sources/utils/gitEngine.py
@@ -20,40 +20,51 @@ import os
 import codecs
 import subprocess
 
+subprocessThrowException = False
+
 class GitEngine():
 
     def __init__(self, path):
         self._path = path
+        self._ok = self.isInGitRepository()
 
     def isInGitRepository(self):
-        if not os.path.exists(os.path.join(self._path, ".git")):
-            return False
-        return True
+        return os.path.exists(os.path.join(self._path, ".git"))
 
     def createGitignore(self):
         if os.path.isfile(os.path.join(self._path, '.gitignore')): return
         f = open(os.path.join(self._path, '.gitignore'), 'w')
-        gitignore = '*.ufo'
+        gitignore = '''*.ufo
+        locker__
+        '''
         f.write(gitignore)
+        f.close()
         self.commit('add gitignore')
         self.push()
 
     def commit(self, stamp):
-        if not self.isInGitRepository(): return False
+        if not self._ok: return False
         comment =  str(stamp)
-        subprocess.call(['git', 'add', '.', self._path], cwd=self._path)
-        subprocess.call(['git', 'commit', '-am', comment], cwd=self._path)
+        #subprocess.call(['git', 'add', '.', self._path], cwd=self._path)
+        # check=True will raise exception of the process does not terminates properly
+        cp = subprocess.run(['git', 'add', '.'], cwd=self._path, check=subprocessThrowException) # In case there are new files?
+        if cp.returncode != 0: return False
+        cp = subprocess.run(['git', 'commit', '-am', comment], cwd=self._path, check=subprocessThrowException)
+        if cp.returncode != 0: return False
         return True
-        
+
     def push(self):
-        subprocess.call(['git', 'push'], cwd=self._path)
+        if not self._ok: return
+        cp = subprocess.run(['git', 'push'], cwd=self._path, check=subprocessThrowException)
+        return cp.returncode == 0
 
     def pull(self):
-        if not self.isInGitRepository(): return False
-        subprocess.call(['git', 'pull'], cwd=self._path)
-        return True
+        if not self._ok: return False
+        cp = subprocess.run(['git', 'pull'], cwd=self._path, check=subprocessThrowException)
+        return cp.returncode == 0
 
     def user(self):
-        return codecs.decode(subprocess.check_output(
-                ['git', 'config', 'user.name'], 
+        u = codecs.decode(subprocess.check_output(
+                ['git', 'config', 'user.name'],
                 cwd=self._path), 'utf-8')
+        return u[:-1].replace(' ','_')
