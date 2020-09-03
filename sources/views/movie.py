c63b04f214426d0aac08b3c67e9ec4be2ebec41b
Gaëtan Baehr <gaetan.baehr@black-foundry.com>
2020-09-03 17:30:16 +0200

    speeded up per 20 times the make movies

--- sources/views/movie.py
+++ sources/views/movie.py
@@ -30,6 +30,8 @@ from fontTools.ufoLib.pointPen import PointToSegmentPen
 from pathlib import Path
 from xml.etree import ElementTree as ET
 from utils import files
+from utils import interpolation
+from mojo.roboFont import NewFont
 try:
     import drawBot as db
 except:
@@ -38,11 +40,13 @@ except:
 
 from imp import reload
 import copy
+import time
    
 class Movie:
 
     def __init__(self, RCJKI):
         self.RCJKI = RCJKI
+        start = time.time()
         ufo = self.RCJKI.currentFont
     # ufo = RCJKFont('/Users/jeremiehornus/Documents/GIT/TYPE/gscjkrcjk/Hanzi.rcjk')
         gname = self.RCJKI.currentGlyph.name
@@ -60,13 +64,15 @@ class Movie:
             #     if gname in layer and layer.name not in ['foreground', 'mask']:
             #         axes.append(layer.name)
             # axes = keepCompatibleLayers(ufo[gname], axes)
-            self.makeInstance(axes, gname)
+            self.makeInstance(axes, gname, glyph)
                  
         #saveImage('deepcompo.gif', imageResolution=144)
         outputPath = os.path.join(self.RCJKI.currentFont.fontPath, "Proofing", '%s.mp4'%gname)
         files.makepath(outputPath)
         # outputPath = '/Users/gaetanbaehr/Documents/BlackFoundry/%s.mp4'%gname
         db.saveImage(outputPath, imageResolution=144)
+        stop = time.time()
+        print(stop-start, 'for all movie')
 
     def checkEqual(self, l):
         for i, e in enumerate(l):
@@ -106,7 +112,7 @@ class Movie:
     #     glyph.draw(pen)
     #     db.drawPath(pen.path)
 
-    def makeInstance(self, axes, gname):
+    def makeInstance(self, axes, gname, glyph):
         nbAxes = maxNbAxes = len(axes)
         # steps = [1, 2]
         # maxNbAxes = max(steps)
@@ -122,17 +128,24 @@ class Movie:
         #     print(speeds, LCM, (LCM < 600 or LCM > 1200))
      
         alpha = 2*math.pi/maxNbAxes
+        ld = [{'Axis': l, 'PreviewValue':0} for l in axes]
+        glyph.preview.computeDeepComponentsPreview(ld)
+        origineGlyph = glyph.preview.variationPreview.copy()
+        origineGlyph.name = "interpo"
         db.newDrawing()
         for k in range(nbAxes):
+            start = time.time()
+            ld = [{'Axis': l, 'PreviewValue':[0, 1][j == k]} for j, l in enumerate(axes)]
+            glyph.preview.computeDeepComponentsPreview(ld)
             for g in range(LCM):
 
                 H = 700
                 W = 700
-                db.newPage(W*2, H)
+                db.newPage(W*2.5, H)
                 db.frameDuration(1/30)
 
                 db.fill(1)
-                db.rect(0, 0, W*2, H)
+                db.rect(0, 0, W*2.5, H)
 
                 r = W/3
                 ainc = 0
@@ -185,19 +198,20 @@ class Movie:
                     ftxt = db.FormattedString(txt=lineName, font="GrtskZetta-Light", fontSize=14, align="center")
                     db.textBox(ftxt, (H/2+line[0]*1.3-30, W/2+line[1]*1.3-10, 60, 20))
 
-
+                #########
                 db.save()
-                ld = []
-                for j, l in enumerate(axes):
-                    ld.append({'Axis': l, 'PreviewValue':rands[j][g]/1000})
+                # ld = []
+                # for j, l in enumerate(axes):
+                #     ld.append({'Axis': l, 'PreviewValue':rands[j][g]/1000})
                     
-                # d = {l:rands[j][g]/1000 for (j, l) in enumerate(axes)}
+                # # d = {l:rands[j][g]/1000 for (j, l) in enumerate(axes)}
 
-                # glyph = deepolation(NewFont().newGlyph('temp'), ufo[gname], layersInfo = d)
-                glyph = self.RCJKI.currentFont[gname]
-                glyph.preview.computeDeepComponentsPreview(ld)
+                # # glyph = deepolation(NewFont().newGlyph('temp'), ufo[gname], layersInfo = d)
+                # # glyph = self.RCJKI.currentFont.get(gname)
+                # glyph.preview.computeDeepComponentsPreview(ld)
+                #########
                 # print(glyph)
-                db.translate(W*1.15, H*.15)
+                db.translate(W*1.3, H*.15)
                 db.scale(.7*H/1000)
                 db.stroke(.5)
                 db.fill(None)
@@ -207,7 +221,10 @@ class Movie:
                 db.save()
                 db.translate(0, 120)
 
-                db.drawGlyph(glyph.preview.variationPreview)
+                ratioX = ratioY = (rands[k][g])/1000
+                interpoGlyph = interpolation.interpol_glyph_glyph_ratioX_ratioY_scaleX_scaleY(origineGlyph, glyph.preview.variationPreview, ratioX, ratioY, 1, 1, NewFont(showUI = False))
+                db.drawGlyph(interpoGlyph)
+
         #         for aes in glyph.preview:
         #             # axis2layerName = {axisName:layerName for axisName, layerName in self.RCJKI.currentFont[aes['name']].lib['robocjk.atomicElement.glyphVariations'].items()}
                         
@@ -223,6 +240,9 @@ class Movie:
                 db.restore()
                 caption = db.FormattedString(txt='%s-axis' %(nbAxes), font="GrtskMega-Medium", fontSize=14, align="left")
                 db.textBox(caption, (10, 10, W-20, 20))
+
+            stop = time.time()
+            print(stop-start, "seconde for axis")
 
         pdfData = db.pdfImage()
 
