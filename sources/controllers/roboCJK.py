e487cfed3d961320fe576b70df0155ae18433f77
Gaëtan Baehr <gaetan.baehr@black-foundry.com>
2021-01-22 17:17:43 +0100

    upgraded version

--- sources/controllers/roboCJK.py
+++ sources/controllers/roboCJK.py
@@ -114,7 +114,7 @@ class RoboCJKController(object):
     hiddenSavePath = os.path.join(NSSearchPathForDirectoriesInDomains(14, 1, True)[0], APPNAME, 'mySQLSave')
     files.makepath(hiddenSavePath)
 
-    _version = 1.1
+    _version = 1.2
 
     def __init__(self):
         self.observers = False
